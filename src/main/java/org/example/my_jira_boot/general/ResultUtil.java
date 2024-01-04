1bfe40ea763b9841a4d23c1ad27b9ff6cababa42
泷川一 <2463640722@qq.com>
2024-01-05 01:57:53 +0800

    feat: - -

--- src/main/java/org/example/my_jira_boot/general/ResultUtil.java
+++ src/main/java/org/example/my_jira_boot/general/ResultUtil.java
@@ -3,30 +3,37 @@ package org.example.my_jira_boot.general;
 public class ResultUtil {
     public static <T> Result<T>  defineSuccess(Integer code, T data) {
         Result result = new Result<>();
-        return result.setCode(code).setData(data);
+        result.setCode(code);
+        result.setData(data);
+        return result;
     }
 
     public static <T> Result<T> success(T data) {
         Result result = new Result();
-        result.setCode(ResultEnum.SUCCESS.code).setData(data);
+        result.setCode(ResultEnum.SUCCESS.code);
+        result.setData(data);
         return result;
     }
 
     public static <T> Result<T> fail(String msg) {
         Result result = new Result();
-        result.setCode(ResultEnum.FAIL).setMsg(msg);
+        result.setCode(ResultEnum.FAIL.code) ;
+        result.setMsg(msg);
         return result;
     }
 
     public static <T> Result<T> defineFail(int code, String msg){
         Result result = new Result();
-        result.setCode(code).setMsg(msg);
+        result.setCode(code);
+        result.setMsg(msg);
         return result;
     }
 
     public static <T> Result<T> define(int code, String msg, T data){
         Result result = new Result();
-        result.setCode(code).setMsg(msg).setData(data);
+        result.setCode(code);
+        result.setMsg(msg);
+        result.setData(data);
         return result;
     }
 }
